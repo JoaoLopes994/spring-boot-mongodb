3ea98fd215b094ba65bd7a932e82a658703abadd
João Pedro Silva <joao.silva@conexasaude.com.br>
2021-05-27 17:52:51 -0300

    Consulta com vários critérios

--- src/main/java/com/joaosilva/workshopmongo/services/PostService.java
+++ src/main/java/com/joaosilva/workshopmongo/services/PostService.java
@@ -1,7 +1,11 @@
 package com.joaosilva.workshopmongo.services;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,4 +29,11 @@ public class PostService {
 	public List<Post> finByTitle(String text){
 		return postRepo.searchTitle(text);
 	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepo.fullSearch(text, minDate, maxDate);
+	}
+	
+
 }
