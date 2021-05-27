16a0739086cfbbb1c53716da0ad78a8f898689fa
João Pedro Silva <joao.silva@conexasaude.com.br>
2021-05-27 15:49:47 -0300

    Consulta com query methods

--- src/main/java/com/joaosilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/joaosilva/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.joaosilva.workshopmongo.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,9 @@ import com.joaosilva.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 
 	
 }
--- src/main/java/com/joaosilva/workshopmongo/services/PostService.java
+++ src/main/java/com/joaosilva/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.joaosilva.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	
+	public List<Post> finByTitle(String text){
+		return postRepo.findByTitleContainingIgnoreCase(text);
+	}
 }
